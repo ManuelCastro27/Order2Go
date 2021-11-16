657d5b82eb54b285b91a814e152b2c51bc69d37f
manuel.castro <manuel.castro@uned.cr>
2021-11-15 19:05:52 -0600

    Instalacion y configuracion de paquetes y de base de datos, ademas de los modelos de proyecto

---
+++ Alojamiento/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Alojamiento.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alojamiento
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurante> Restaurante { get; set; }
+        public DbSet<Producto> Producto { get; set; }
+        public DbSet<Venta> Venta { get; set; }
+
+    }
+}
